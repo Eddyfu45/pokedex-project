92620c0b0c7124f25c6bb032e281c990be882824
Eddyfu45 <eddyfu45@yahoo.com>
2021-05-24 01:20:16 -0400

    Search Query Done

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -2,19 +2,28 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const { route } = require('./auth');
 // const fetch = require('node-fetch');
 // const fs = require('fs');
+router.get('/new', (req, res) => {
+    res.render('pokemon/new');
+})
 
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
+    if (req.query.nameFilter) {
+        const searchQuery = req.query.nameFilter;
+        const searchPoke = await db.pokemon.findOne({ where: {name: searchQuery} });
+        if (searchPoke) {
+            res.redirect(`/pokemon/${searchPoke.id}`);
+        } else {
+            res.redirect('/pokemon');
+        }
+    } else (
     db.pokemon.findAll()
     .then((results) => {
         res.render('pokemon/index', { pokemon: results})
-    })
+    }));
 });
-
-// router.post('/', function(req, res) {
-//     res.render('pokemon/index')
-// })
 
 router.get("/:idx", (req,res)=>{
     const pokeIndex = parseInt(req.params.idx);
@@ -29,16 +38,42 @@ router.get('/edit/:idx', (req, res) => {
     res.render('pokemon/edit', { pokeId: editIndex });
 });
 
-router.put('/:idx', (req, res) => {
+router.get('/new', (req, res) => {
+    console.log("going into new");
+    res.render('pokemon/new');
+})
+
+router.put('/:idx', async (req, res) => {
     console.log("This is put");
     const pokePick = parseInt(req.params.idx);
-    pokeEdit = db.pokemon.findOne({ where: {id: pokePick } })
-    .then(pokeSelect => {
-        const { name, types } = req.body;
-        pokeSelect.name = name;
-        pokeSelect.types = types;
-        res.redirect(`/pokemon/${pokeSelect}`);
-    })
+    const pokeEdit = await db.pokemon.findOne({ where: {id: pokePick } })   
+    const { name, type } = req.body;
+    pokeEdit.name = name;
+    pokeEdit.types = type;
+    await pokeEdit.save();
+    res.redirect(`/pokemon/${pokePick}`);
 })
+
+router.post('/', async (req, res) => {
+    newPokeName = req.body.name;
+    newPokeType = req.body.type;
+    newPokeImage = req.body.image;
+    const newPokemon = {
+        name: newPokeName,
+        types: newPokeType,
+        image: newPokeImage
+    }
+    const createPoke = await db.pokemon.create(newPokemon);
+    res.redirect('/pokemon');
+})
+
+router.delete('/edit/:idx', async(req, res) => {
+    const delIndex = req.params.idx;
+    const deletePoke = await db.pokemon.destroy({ where: {id: delIndex }
+    })
+    res.redirect('/pokemon');
+})
+
+
 
 module.exports = router;
